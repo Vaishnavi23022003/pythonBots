eda22a9a7c7b4b5d2fdb5c0ce1b1b733aad3a049
Vaishnavi23022003 <73733057+Vaishnavi23022003@users.noreply.github.com>
2022-12-09 00:39:08 +0530

    Update temp.py

--- CrackCP/temp.py
+++ CrackCP/temp.py
@@ -1,6 +1,6 @@
 import requests
 
-sheety=requests.get('https://api.sheety.co/aeffad5a770fb5eb8147b688c81c7747/crackTeBe/sheet1')
+sheety=requests.get('https://api.sheety.co/===================/crackTeBe/sheet1')
 
 handle = [i['codeForcesHandle'] for i in sheety.json()['sheet1']]
 print(handle)
