3f991be029470120a5c7a528eee507b14a1df409
Vaishnavi23022003 <73733057+Vaishnavi23022003@users.noreply.github.com>
2022-12-09 00:36:43 +0530

    Update data_manager.py

--- Find_cheap_flights/data_manager.py
+++ Find_cheap_flights/data_manager.py
@@ -1,12 +1,12 @@
 import requests
 from flight_data import FlightData
 
-SHEETY_URL = "https://api.sheety.co/aeffad5a770fb5eb8147b688c81c7747/flightDeals"
+SHEETY_URL = "https://api.sheety.co/=======================/flightDeals"
 
 class DataManager:
 
     def __init__(self,f_name,l_name,mail):
-        res = requests.get(url=f"{SHEETY_URL}/prices", headers={"Authorization": "Bearer killuavaab"})
+        res = requests.get(url=f"{SHEETY_URL}/prices", headers={"Authorization": "==================="})
         self.flight_data = res.json()["prices"]
         self.find_iata()
         if f_name!="":
@@ -17,8 +17,8 @@ class DataManager:
                     "email":mail
                 }
             }
-            res=requests.post(url=f"{SHEETY_URL}/users",headers={"Authorization": "Bearer killuavaab"},json=data)
-        res=requests.get(url=f"{SHEETY_URL}/users",headers={"Authorization": "Bearer killuavaab"})
+            res=requests.post(url=f"{SHEETY_URL}/users",headers={"Authorization": "====================="},json=data)
+        res=requests.get(url=f"{SHEETY_URL}/users",headers={"Authorization": "====================="})
         self.usr_data=[{"firstName": i["firstName"], "lastName": i["lastName"], "email": i["email"]} for i in res.json()["users"]]
 
     def find_iata(self):
